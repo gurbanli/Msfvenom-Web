e8ca7a2e51ddaa75cce42e8c394257c10b4a10f0
gurbanli <gurbanliaydin@gmail.com>
2020-04-13 23:49:51 +0400

    Changed tag name

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-docker image build -t msfvenom_web:local .
-docker container run -d -p 8080:5000 msfvenom_web
+docker image build -t msfvenom_online:local .
+docker container run -d -p 8080:5000 msfvenom_online
 
 echo "Successfully created container. Check 127.0.0.1:8080"
